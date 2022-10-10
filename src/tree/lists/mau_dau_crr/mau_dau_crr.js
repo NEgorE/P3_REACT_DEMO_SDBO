98cc2b91c984a5487d7880be66cb628edb6800e5
Egor Neliubin <neo.8318731@gmail.com>
2022-10-10 16:55:38 +0300

    print 1st report 3

--- src/tree/lists/mau_dau_crr/mau_dau_crr.js
+++ src/tree/lists/mau_dau_crr/mau_dau_crr.js
@@ -1,28 +1,28 @@
-import React, { PureComponent, useEffect, useState} from "react";
+import React, { PureComponent, useEffect, useState, useRef} from "react";
+import { createRoot } from 'react-dom/client';
 
-import { LineChart, BarChart, Line, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Brush, ResponsiveContainer, LabelList } from 'recharts';
+import { LineChart, BarChart, Line, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Brush, ResponsiveContainer, LabelList, ReferenceLine, Label } from 'recharts';
 
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./mau_dau_crr.css";
 
-class CustomizedLabel extends PureComponent {
-    render() {
-
-        console.log(this.props);
-        const { x, y, stroke, value, width, height } = this.props;
-    
-        return (
-            <text x={x + width/2} y={y+height/2}  fill={stroke}  textAnchor="middle" fill="#fff">
-            {value}
-            </text>
-        );
-    }
-  }
-
 export const Maudaucrr = () => {
 
+    class CustomizedLabelChart2 extends PureComponent {
+        
+        render() {
+            const { x, y, value, index} = this.props;
+            console.log(this.props);
+            const delta = 0;
+            return (
+                    <text x={x} y={y} textAnchor="middle" value={value} id={`Chart2_index${index}`} >{value} </text>
+            )
+        }
+      }
+
     const [chart2, setChart2] = useState(false)
+    const [chart4, setChart4] = useState(false)
     const [chart2data, setchart2data] = useState(false)
     const [chart3, setChart3] = useState(false)
     const [chart3data, setchart3data] = useState(false)
@@ -70,9 +70,7 @@ export const Maudaucrr = () => {
                     <YAxis domain={[dMin, dMax]} />
                     <Tooltip />
                     <Brush dataKey='date_year_month' height={10} />
-                    <Line type="monotone" dataKey="mau" stroke="#4477aa" fill="#4477aa" strokeDasharray="10 5" strokeWidth={2} dot={{ r: 5 }} label="mau" >
-                        <LabelList dataKey="mau" position="top" />
-                    </Line>
+                    <Line type="monotone" dataKey="mau" stroke="#4477aa" fill="#4477aa" strokeDasharray="10 5" strokeWidth={2} dot={{ r: 5 }} label={<CustomizedLabelChart2 />} />
                 </LineChart>
             </ResponsiveContainer>
         ); 
@@ -98,16 +96,42 @@ export const Maudaucrr = () => {
                     <Tooltip />
                     <Legend verticalAlign="top" />
                     <Brush dataKey='date_year_month' height={10} />
-                    <Bar dataKey="Mikro" stackId={"a"}  fill="#3a9058" unit={'%'} label={<CustomizedLabel />}>
-                    </Bar>
-                    <Bar dataKey="Maliy" stackId={"a"}  fill="#301c89"  unit={'%'} ><LabelList dataKey="Maliy" position="center"/></Bar>
-                    <Bar dataKey="Sredniy" stackId={"a"}  fill="#cd6678"  unit={'%'} ><LabelList dataKey="Sredniy" position="center"/></Bar>
+                    <Bar dataKey="Mikro" stackId={"a"}  fill="#3a9058" unit={'%'} ><LabelList dataKey="Mikro" position="center" fill="#fff"/></Bar>
+                    <Bar dataKey="Maliy" stackId={"a"}  fill="#301c89"  unit={'%'} ><LabelList dataKey="Maliy" position="center" fill="#fff"/></Bar>
+                    <Bar dataKey="Sredniy" stackId={"a"}  fill="#cd6678"  unit={'%'} ><LabelList dataKey="Sredniy" position="center" fill="#fff"/></Bar>
                     <Bar dataKey="Krupniy" stackId={"a"}  fill="#decd78"  unit={'%'} ><LabelList dataKey="Krupniy" position="center"/></Bar>
                     <Bar dataKey="Krupneyshiy" stackId={"a"}  fill="#89cdef"  unit={'%'} ><LabelList dataKey="Krupneyshiy" position="center"/></Bar>
                 </BarChart>
             </ResponsiveContainer>
         ); 
         setChart3(element);
+    }
+
+    function generateChart4(data) {
+        const dMin = Math.round((Math.min(...data.filter(item => item.crr != null).map(o => o.crr)) - Math.min(...data.filter(item => item.crr != null).map(o => o.crr))*0.02))
+        const dMax = Math.round((Math.max(...data.filter(item => item.crr != null).map(o => o.crr)) + Math.min(...data.filter(item => item.crr != null).map(o => o.crr))*0.02))
+        const element = (
+            <ResponsiveContainer width={'100%'} height={'100%'}>
+                <LineChart
+                    data={data}
+                    margin={{
+                    top: 5,
+                    right: 30,
+                    left: 10,
+                    bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="date_year_month" angle={-25} interval={0} tickSize={11} height={40}/>
+                    <YAxis domain={[dMin, dMax]} unit={"%"}/>
+                    <Tooltip />
+                    <ReferenceLine y={100} label={{ value: '100%', position: 'insideTopLeft', fill:'red' }} stroke="red" strokeDasharray="3 3" />
+                    <Brush dataKey='date_year_month' height={10} />
+                    <Line type="monotone" dataKey="crr" stroke="#4477aa" fill="#4477aa" strokeDasharray="10 5" strokeWidth={2} dot={{ r: 5 }} label={<CustomizedLabelChart2 />} />
+                </LineChart>
+            </ResponsiveContainer>
+        ); 
+        setChart4(element);
     }
     
     useEffect(() => {
@@ -118,8 +142,10 @@ export const Maudaucrr = () => {
     useEffect(() => {
         if(chart2data){
             generateChart2(chart2data);
+            generateChart4(chart2data);
         }
     }, [chart2data])
+
     useEffect(() => {
         if(chart3data){
             generateChart3(chart3data);
@@ -158,7 +184,7 @@ export const Maudaucrr = () => {
                 </div>
                 <div class='col col-4 h-100 cobj' >
                     <div class='col obj h-100'>
-                        { chart2 ? chart2 : 'Smth wrong' }
+                        { chart4 ? chart4 : 'Smth wrong' }
                     </div>
                 </div>
             </div>
