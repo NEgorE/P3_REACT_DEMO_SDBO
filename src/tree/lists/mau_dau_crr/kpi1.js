0e85db856dc7a3361a6aad6a2aea7b03d90e6fb5
Egor Neliubin <neo.8318731@gmail.com>
2022-10-31 12:22:57 +0300

    add reactions by filter 3

--- src/tree/lists/mau_dau_crr/kpi1.js
+++ src/tree/lists/mau_dau_crr/kpi1.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { subscriberMetric1 } from '../../../MessageService.js';
+import { subscriberMetric1 , subscriberFilter1 } from '../../../MessageService.js';
 import { METRICS } from '../../components/constants';
 
 export const Kpi1 = (props) => {
@@ -7,6 +7,7 @@ export const Kpi1 = (props) => {
     const log_prefix = 'KPI1: '
 
     const currMetric = subscriberMetric1._value
+    const currFilter1 = subscriberFilter1._value
 
     const [kpi1, setKpi1] = useState(false)
     const [chart2data, setchart2data] = useState(false)
@@ -20,18 +21,37 @@ export const Kpi1 = (props) => {
         if(chart2data){
             generateKpi1(chart2data, METRICS, currMetric);
         }
-    }, [chart2data, currMetric])
+    }, [chart2data])
+
+    useEffect(() => {
+        getChart2Data();
+    }, [currFilter1, currMetric])
     
     function getChart2Data() {
         let result = false;
-        fetch(`http://localhost:3001/select_mau`)
-          .then(response => {
-            return response.text();
-          })
-          .then(data => {
-            result = JSON.parse(data);
-            setchart2data(result);
-          });
+        console.log(log_prefix + currFilter1.length + ' length filter');
+        if ( currFilter1.length <= 0 ) {
+            fetch(`http://localhost:3001/select_mau`)
+            .then(response => {
+                return response.text();
+            })
+            .then(data => {
+                result = JSON.parse(data);
+                setchart2data(result);
+            });
+        }
+        else {
+            const query = `http://localhost:3001/select_mau_by_filters/filter1=${currFilter1}`
+            console.log(log_prefix + query);
+            fetch(query)
+            .then(response => {
+                return response.text();
+            })
+            .then(data => {
+                result = JSON.parse(data);
+                setchart2data(result);
+            });
+        }
     };
 
     function generateKpi1(data, METRICS, currMetric) {
