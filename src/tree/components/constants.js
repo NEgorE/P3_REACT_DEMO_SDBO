553b0166fcce1f365c2fa2cf473b4a7120e0f5a3
Egor Neliubin <neo.8318731@gmail.com>
2022-12-02 16:14:47 +0300

    1st commit 2gen

--- src/tree/components/constants.js
+++ src/tree/components/constants.js
@@ -1,10 +1,11 @@
 import { Maudaucrr } from '../lists/mau_dau_crr/mau_dau_crr.js';
 import { Services } from '../lists/services/services.js';
+import { Sessions } from '../lists/sessions/sessions.js';
 
 export const TABS = [
     {title: 'MAU/DAU/CRR', type: 1, obj: <Maudaucrr current_list_name='maudaucrr'/>},
     {title: 'Services', type: 2, obj: <Services />},
-    {title: 'Sessions', type: 3},
+    {title: 'Sessions', type: 3, obj: <Sessions />},
     {title: 'Add next tab...', type: 4},
   ]
 
